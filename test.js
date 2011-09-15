ad95b2c7929eef6b3b7d0ea84d3d47ca6dffa4f5
Rafal Kukawski <rafal@kukawski.pl>
2011-09-15 22:02:53 +0200

    Preparing unit tests

---
+++ test.js
@@ -0,0 +1,165 @@
+module("Parser");
+test("Testing the parser", function () {
+    
+});
+
+module("Day");
+test("d modifier", function () {
+
+});
+
+test("D modifier", function () {
+
+});
+
+test("j modifier", function () {
+
+});
+
+test("l modifier", function () {
+
+});
+
+test("N modifier", function () {
+
+});
+
+test("S modifier", function () {
+
+});
+
+test("w modifier", function () {
+
+});
+
+test("z modifier", function () {
+
+});
+
+
+module("Week");
+test("W modifier", function () {
+
+});
+
+module("Month");
+test("F modifier", function () {
+
+});
+
+test("m modifier", function () {
+
+});
+
+test("M modifier", function () {
+
+});
+
+test("n modifier", function () {
+
+});
+
+test("t modifier", function () {
+
+});
+
+module("Year");
+test("L modifier", function () {
+
+});
+
+test("o modifier", function () {
+
+});
+
+test("Y modifier", function () {
+
+});
+
+test("y modifier", function () {
+
+});
+
+module("Time");
+test("a modifier", function () {
+
+});
+
+test("A modifier", function () {
+
+});
+
+test("B modifier", function () {
+
+});
+
+test("g modifier", function () {
+
+});
+
+test("G modifier", function () {
+
+});
+
+test("h modifier", function () {
+
+});
+
+test("H modifier", function () {
+
+});
+
+test("i modifier", function () {
+
+});
+
+test("s modifier", function () {
+
+});
+
+test("u modifier", function () {
+
+});
+
+module("Timezone");
+test("e modifier", function () {
+
+});
+
+test("I modifier", function () {
+
+});
+
+test("O modifier", function () {
+
+});
+
+test("P modifier", function () {
+
+});
+
+test("T modifier", function () {
+
+});
+
+test("Z modifier", function () {
+
+});
+
+module("Full date and time");
+test("c modifier", function () {
+
+});
+
+test("r modifier", function () {
+
+});
+
+test("U modifier", function () {
+
+});
+
+module("Predefined templates");
+test("Testing constants", function () {
+
+});
